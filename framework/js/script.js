4e93180db1f90e5704138e06b15d1fce5a25171a
nicolas <nikolasbaele@gmail.com>
2023-02-07 16:50:27 +0100

    ajout titre v1

--- framework/js/script.js
+++ framework/js/script.js
@@ -1,11 +1,2 @@
-$(document).ready(function() {
-    let title = this.querySelectorAll('.secondaryTitle');
-    title.forEach(element => {
-        let width = element.offsetWidth;
-        console.log(width);
-        //$(element>'.line').width(width);
-        element.querySelector('.line').style.width = element.offsetWidth;
-    });
-});
 
 
